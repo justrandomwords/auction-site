4b6456840de13032bf86d15345e65085da85b1c5
Mifaresss <9803211@gmail.com>
2024-02-10 15:27:20 +0200

    chore: add prettier, fix code by prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-import { cardsData } from "@/tempData";
-import AuctionCard from "./_components/AuctionCard";
+import { cardsData } from '@/tempData'
+import AuctionCard from './_components/AuctionCard'
 
 export default function Home() {
-  return (
-    <div className='auctions-container'>
-      {cardsData.map(element => 
-        <AuctionCard
-          id='test'
-          title={element.title}
-          price={element.price}
-          time={element.time} 
-        />)
-      }
-    </div>
-  );
+	return (
+		<div className='auctions-container'>
+			{cardsData.map(element => (
+				<AuctionCard
+					key={element.title}
+					id='test'
+					title={element.title}
+					price={element.price}
+					time={element.time}
+				/>
+			))}
+		</div>
+	)
 }
