44d457f2d8f61c45e8cf709560bd7cb3fb98b3a3
justrandomwords <134196541+justrandomwords@users.noreply.github.com>
2024-02-10 14:54:13 +0200

    feat: add auction page template

---
+++ src/app/item/page.tsx
@@ -0,0 +1,26 @@
+export default function() {
+  return (
+    <div className='grid grid-cols-2'>
+      <div></div>
+      <div>
+        <h3>Кулер для процесора Be quiet! Pure Rock 2 Black (115X/1200/2011v3/2066/AM2-AM4/1700) (BK007)</h3>
+        <div className='flex justify-between'>
+          <div className=''>
+            <div>Current bet</div>
+            <div className='flex'>
+              <p className='text-xl font-bold'>{'1 015'} $</p>
+              <p>({'3'} bids)</p>
+            </div>
+            <div>Current bettor: {'Бандера'}</div>
+          </div>
+          <div className='flex flex-col items-end'>
+            <p>Time left</p>
+            <p className='text-xl font-semibold'>12:02:05</p>
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
